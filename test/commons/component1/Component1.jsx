62d5e1e47e326598b778a07e829eccaa4bbb6ca4
liujunjie <liujunjie@bbdservice.com>
2019-03-04 11:52:23 +0800

    quick commit

--- test/commons/component1/Component1.jsx
+++ test/commons/component1/Component1.jsx
@@ -10,7 +10,7 @@ test('Component1 changes the text after click', () => {
     const checkbox = shallow(<Component1 labelOn="On" labelOff="Off" />);
 
     expect(checkbox.text()).toEqual('Off');
-
+    // trigger event
     checkbox.find('input').simulate('change');
 
     expect(checkbox.text()).toEqual('On');
