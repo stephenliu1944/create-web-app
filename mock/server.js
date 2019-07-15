7cb17fbad8ca3b8a35b813c820c7ed8fefb3bbcc
liujunjie <liujunjie@bbdservice.com>
2019-07-15 19:59:51 +0800

    mock server部分代码

--- mock/server.js
+++ mock/server.js
@@ -1,14 +1,31 @@
 // server.js
+var path = require('path');
 var jsonServer = require('json-server');
 var pkg = require('../package.json');
-
-const server = jsonServer.create();
-const router = jsonServer.router('mock/data/db.json');
-const middlewares = jsonServer.defaults();
 var { mock } = pkg.devEnvironments.servers;
 
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'data/db.json'));
+const middlewares = jsonServer.defaults();
+
+// Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
+
+// In this example, returned resources will be wrapped in a body property
+router.render = (req, res) => {
+    res.jsonp({
+        body: res.locals.data
+    });
+};
+
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}));
+  
+// Use default router
 server.use(router);
 server.listen(mock, () => {
-    console.log('JSON Server is running');
+    console.log('Mock Server is running');
 });
